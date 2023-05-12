2ed4624f98b58988153780c9ac97ecc5bbcfe831
Reinaldo da Silva Vargas <rsv.snf17@dmz.org.br>
2023-05-12 17:07:56 -0300

    SEGUNDA IMPLEMENTACAO

--- src/redis/redis-module.module.ts
+++ src/redis/redis-module.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { RedisModuleController } from './redis-module.controller';
-import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { CacheInterceptor, CacheModule, CacheStore } from '@nestjs/cache-manager';
 import { PokemonService } from './redis-module.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-
+import * as redisStore from "cache-manager-redis-store";
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [CacheModule.register({
-    
-  })],
-  providers: [PokemonService, {provide: APP_INTERCEPTOR, useClass: CacheInterceptor}],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      url: "redis://localhost:6379",
+    })],
+  providers: [PokemonService, { provide: APP_INTERCEPTOR, useClass: CacheInterceptor }],
   controllers: [RedisModuleController],
 })
 export class RedisModules { }
